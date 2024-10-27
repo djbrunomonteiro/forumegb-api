84a1a14fa544494929c42e469f446f56c407a61d
djbrunomonteiro <brunomonteiroestudio@gmail.com>
2024-10-27 03:34:42 -0300

    chore: corrige via lint

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,7 +1,7 @@
 import { IsEmail, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateUserDto {
-  @IsOptional()  // Este campo não deve ser fornecido durante a criação
+  @IsOptional() // Este campo não deve ser fornecido durante a criação
   @IsNumber()
   id?: number;
 
@@ -11,34 +11,34 @@ export class CreateUserDto {
   @IsEmail()
   email: string;
 
-  @IsOptional()  // Pode ser opcional
+  @IsOptional() // Pode ser opcional
   @IsString()
   photoURL?: string;
 
-  @IsOptional()  // Pode ser opcional
+  @IsOptional() // Pode ser opcional
   @IsString()
   social_links?: string;
 
   @IsString()
   permission: string;
 
-  @IsOptional()  // Pode ser opcional
+  @IsOptional() // Pode ser opcional
   @IsString()
   metadata?: string;
 
-  @IsOptional()  // Pode ser opcional
+  @IsOptional() // Pode ser opcional
   @IsString()
   end_uf?: string;
 
-  @IsOptional()  // Pode ser opcional
+  @IsOptional() // Pode ser opcional
   @IsString()
   end_city?: string;
 
-  @IsOptional()  // Pode ser opcional
+  @IsOptional() // Pode ser opcional
   @IsString()
   created_at?: string;
 
-  @IsOptional()  // Pode ser opcional
+  @IsOptional() // Pode ser opcional
   @IsString()
   updated_at?: string;
 }
