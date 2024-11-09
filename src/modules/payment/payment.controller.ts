f581a37aa719b62a1794e36ca1076dfd51210561
djbrunomonteiro <brunomonteiroestudio@gmail.com>
2024-11-09 01:53:39 -0300

    feat(payment): valida origem webhook

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Headers, BadRequestException } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { CreatePaymentDto } from './dto/create-payment.dto';
 import { UpdatePaymentDto } from './dto/update-payment.dto';
+import * as crypto from 'crypto';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('payment')
 export class PaymentController {
-  constructor(private readonly paymentService: PaymentService) {}
+  KEYWEBHOOK = ''
+  constructor(
+    private readonly paymentService: PaymentService,
+    private configService: ConfigService,
+  ) {
+    this.KEYWEBHOOK = this.configService.get<string>('KEYWEBHOOKMERCADOPAGO')
+  }
 
   @Post()
   create(@Body() body: any) {
@@ -14,8 +22,33 @@ export class PaymentController {
   }
 
   @Post('mercadopagohooks')
-  listenPayments(@Body() body: any) {
-    console.log('mercadopagohooks', body );
+  listenPayments(@Body() body: any, @Headers('x-request-id') xReqId: string, @Headers('x-signature') xSignature: string) {
+    if (!xSignature) {
+      throw new BadRequestException('Cabeçalho x-signature ausente');
+    }
+    // Separe os valores de timestamp e assinatura (v1) de x-signature
+    const [tsPart, v1Part] = xSignature.split(',');
+    const ts = tsPart?.split('=')[1];
+    const signatureReceived = v1Part?.split('=')[1];
+
+    if (!ts || !signatureReceived) {
+      throw new BadRequestException('x-signature no formato inválido');
+    }
+
+    // Construa a string de assinatura com base nos dados recebidos
+    const signatureString = `id:${body.data.id};request-id:${xReqId};ts:${ts};`;
+
+    // Calcule o HMAC SHA256 com a chave secreta
+    const computedSignature = crypto
+      .createHmac('sha256', this.KEYWEBHOOK)
+      .update(signatureString)
+      .digest('hex');
+
+    // Compare a assinatura calculada com a recebida
+    if (computedSignature !== signatureReceived) {
+      throw new BadRequestException('Assinatura inválida');
+    }
+
     return this.paymentService.getWebHooks(body);
   }
 
@@ -27,6 +60,11 @@ export class PaymentController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.paymentService.findOne(+id);
+  }
+
+  @Get('sync/:id')
+  syncOne(@Param('id') id: string) {
+    return this.paymentService.syncPayment(+id);
   }
 
   @Patch(':id')
